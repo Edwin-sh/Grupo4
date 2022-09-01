7497479110266b693a683d829d62feacdf659b89
Henry Julián <111660032+Henry-Julian@users.noreply.github.com>
2022-09-01 12:58:09 -0500

    Create Vaca.cs

---
+++ BovinoRemoto.App/BovinoRemoto.App.Dominio/Vaca.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BovinoRemoto.dominio
+{
+  pubblic class Vaca
+  {
+    public int Codigo {get;set;}
+    public string Nombre {get;set;}
+    public string Especie {get;set;}
+    public string Raza {get;set;}
+   }
+   
+ }
