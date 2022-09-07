3f416c59b862201cfff4901368a40cc8d7244699
Edwin Martínez <edwinmartinez11032003@gmail.com>
2022-09-07 15:00:11 -0500

    Creacion paginas de navegación

---
+++ BovinoRemoto.App/BovinoRemoto.App.Frontend/Pages/Entidades/Veterinary.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BovinoRemoto.App.Frontend.Pages.Entidades
+{
+    public class VeterinaryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
