1e020273f4c633a3ba7038d5f051c845928b1656
Edwin Martínez <edwinmartinez11032003@gmail.com>
2022-09-08 13:57:27 -0500

    Creacion paginas de acceso y principales

---
+++ BovinoRemoto.App/BovinoRemoto.App.Frontend/Pages/Acceso/LoginOwner.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BovinoRemoto.App.Frontend.Pages.Acceso
+{
+    public class LoginOwnerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
