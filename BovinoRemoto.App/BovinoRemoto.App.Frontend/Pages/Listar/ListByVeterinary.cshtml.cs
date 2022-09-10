a9e6eaabc5763f1937e47debdddaefaa66eb5c08
Edwin Martínez <edwinmartinez11032003@gmail.com>
2022-09-10 00:37:12 -0500

    Correccion error nombres clases

--- BovinoRemoto.App/BovinoRemoto.App.Frontend/Pages/Listar/ListByVeterinary.cshtml.cs
+++ BovinoRemoto.App/BovinoRemoto.App.Frontend/Pages/Listar/ListByVeterinary.cshtml.cs
@@ -3,8 +3,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BovinoRemoto.App.Frontend.Pages.Listar
 {
-    public class ListByVeterynaryModel : PageModel
+    public class ListByVeterinaryModel : PageModel
     {
+
         private string[] Bovinos = { "Bovino a cargo 1", "Bovino a cargo 2", "Bovino a cargo 3" };
         public List<String>? AllBovinos { get; set; }
         public void OnGet()
@@ -12,5 +13,6 @@ namespace BovinoRemoto.App.Frontend.Pages.Listar
             AllBovinos = new List<string>();
             AllBovinos.AddRange(Bovinos);
         }
+
     }
 }
