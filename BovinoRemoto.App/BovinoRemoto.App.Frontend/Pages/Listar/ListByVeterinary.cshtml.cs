7682657ece04766172f6610841a3747448d4bab1
Edwin Martínez <edwinmartinez11032003@gmail.com>
2022-09-16 12:07:36 -0500

    Actualizacion jerarquía

--- BovinoRemoto.App/BovinoRemoto.App.Frontend/Pages/Listar/ListByVeterinary.cshtml.cs
+++ BovinoRemoto.App/BovinoRemoto.App.Frontend/Pages/Listar/ListByVeterinary.cshtml.cs
@@ -8,6 +8,7 @@ namespace BovinoRemoto.App.Frontend.Pages.Listar
 
         private string[] Bovinos = { "Bovino a cargo 1", "Bovino a cargo 2", "Bovino a cargo 3" };
         public List<String>? AllBovinos { get; set; }
+        
         public void OnGet()
         {
             AllBovinos = new List<string>();
