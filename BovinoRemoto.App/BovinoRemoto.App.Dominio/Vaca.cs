d0de7d04ee47ecf77d3054cc492401d091429787
Edwin Martínez <edwinmartinez11032003@gmail.com>
2022-09-17 13:15:45 -0500

    Modificacion entidad Vaca

--- BovinoRemoto.App/BovinoRemoto.App.Dominio/Vaca.cs
+++ BovinoRemoto.App/BovinoRemoto.App.Dominio/Vaca.cs
@@ -6,6 +6,7 @@ namespace BovinoRemoto.App.Dominio
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
+        public string Color { get; set; }
         public string Especie { get; set; }
         public string Raza { get; set; }
         public Dueño Dueño { get; set; }
