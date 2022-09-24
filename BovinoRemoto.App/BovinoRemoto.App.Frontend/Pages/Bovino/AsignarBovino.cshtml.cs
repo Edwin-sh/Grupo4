50d1365fe4eadf1d60903421f0765ed10c203efd
Edwin Martínez <edwinmartinez11032003@gmail.com>
2022-09-24 11:41:09 -0500

    Updt Forms Bovino

--- BovinoRemoto.App/BovinoRemoto.App.Frontend/Pages/Bovino/AsignarBovino.cshtml.cs
+++ BovinoRemoto.App/BovinoRemoto.App.Frontend/Pages/Bovino/AsignarBovino.cshtml.cs
@@ -5,43 +5,31 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BovinoRemoto.App.Frontend.Pages
 {
-    public class ListarPorVeterinarioModel : PageModel
+    public class AsignarBovinoModel : PageModel
     {
         private readonly IRepositorioVeterinario repositorioVeterinario;
         private readonly IRepositorioBovino repositorioBovino;
-
-        public ListarPorVeterinarioModel()
+        public AsignarBovinoModel()
         {
             this.repositorioVeterinario = new RepositorioVeterinario(new Persistencia.AppContext());
             this.repositorioBovino = new RepositorioBovino(new Persistencia.AppContext());
         }
-        [BindProperty]
-        public Veterinario Veterinario { get; set; }
         public IEnumerable<Vaca> Bovinos { get; set; }
         [BindProperty]
         public Vaca Bovino { get; set; }
-
+        [BindProperty]
+        public Veterinario Veterinario { get; set; }
         public void OnGet(int? idveterinario)
         {
-            if (idveterinario != null)
-            {
-                Veterinario = repositorioVeterinario.GetVeterinario(idveterinario.Value);
-                Bovinos = repositorioBovino.GetVacasPorVeterinario(idveterinario.Value);
-            }
-            else
-            {
-                Veterinario = new Veterinario()
-                {
-                    Id = 0
-                };
-            }
+            Veterinario = repositorioVeterinario.GetVeterinario(idveterinario.Value);
+            Bovinos = repositorioBovino.GetVacasSinAsignar();
         }
 
         public IActionResult OnPost()
         {
             var BovinoEncontrado = repositorioBovino.GetVaca(Bovino.Id);
-            repositorioBovino.EliminarVeterinario(Veterinario.Id, BovinoEncontrado);
-            return RedirectToPage("./ListarPorVeterinario", new { idveterinario = Veterinario.Id });
+            repositorioBovino.AsignarVeterinario(Veterinario.Id, BovinoEncontrado);
+            return RedirectToPage("./AsignarBovino", new { idveterinario = Veterinario.Id });
         }
     }
 }
