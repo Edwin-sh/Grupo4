02248bb9cac8d7056ba2acb09bf29b6ed367c685
Edwin Martínez <edwinmartinez11032003@gmail.com>
2022-09-24 11:58:30 -0500

    Updt Forms Dueño

--- BovinoRemoto.App/BovinoRemoto.App.Frontend/Pages/Owner/AccederDueño.cshtml.cs
+++ BovinoRemoto.App/BovinoRemoto.App.Frontend/Pages/Owner/AccederDueño.cshtml.cs
@@ -18,11 +18,10 @@ namespace BovinoRemoto.App.Frontend.Pages
 
         public void OnGet()
         {
-            this.Dueño = new Dueño();
         }
         public IActionResult OnPost()
         {
-            var dEncontrado = repositorioDueño.GetDueñoPorCedula(this.Dueño.Num_Identificacion);
+            var dEncontrado = repositorioDueño.GetDueñoPorCedula(Dueño.Num_Identificacion);
             if (dEncontrado != null)
             {
                 return RedirectToPage("./PrincipalDueño", new { numIdentificacion = Dueño.Num_Identificacion });
